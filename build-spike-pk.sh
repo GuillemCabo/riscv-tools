56078eb6884ab5de7be0b4c4495fd835d544ff8b
Wei Song <wsong83@gmail.com>
2017-09-29 17:14:00 +0100

    bump riscv-tools from upstream commits

--- build-spike-pk.sh
+++ build-spike-pk.sh
@@ -5,13 +5,16 @@
 
 . build.common
 
+if [ ! `which riscv64-unknown-elf-gcc` ]
+then
+  echo "riscv64-unknown-elf-gcc doesn't appear to be installed; use the full-on build.sh"
+  exit 1
+fi
+
 echo "Starting RISC-V Toolchain build process"
 
-build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-build_project riscv-gnu-toolchain --prefix=$RISCV
 CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
-build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf
 
 echo -e "\\nRISC-V Toolchain installation completed!"
