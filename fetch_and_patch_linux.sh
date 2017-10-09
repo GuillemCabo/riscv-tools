b36a4780d86f20ec0513697774b50af132eeb582
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2017-10-09 15:13:37 +0100

    Enable lowrisc-sd in Linux .config

--- fetch_and_patch_linux.sh
+++ fetch_and_patch_linux.sh
@@ -1,4 +1,5 @@
 cd $TOP/riscv-tools
+echo Backing up previous Linux directory \(if any\)
 mkdir -p linux-4.6.2
 mv -f linux-4.6.2{,-old-`date -Isec`}
 curl https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.6.2.tar.xz | tar -xJ
@@ -8,3 +9,7 @@ git remote add origin https://github.com/lowrisc/riscv-linux.git
 git fetch
 git checkout -f -t origin/nominion
 patch -p1 < sdhci_minion_sd.patch
+echo Backing up previous Busybox directory \(if any\)
+mkdir -p busybox-1.21.1
+mv -f busybox-1.21.1{,-old-`date -Isec`}
+curl -L http://busybox.net/downloads/busybox-1.21.1.tar.bz2 | tar -xj
